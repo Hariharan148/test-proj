3fa54148cb05b072925fa2ebee3ef5ce4aed549f
Hariharan <harirevorhustle@gmail.com>
2023-03-16 09:23:28 +0530

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -227,7 +227,7 @@ const IndexPage = () => (
                     {marginBottom: `var(--space-3)`}
                 }/>
             <h1>
-                Welcome to Development server 1
+                Welcome to Development server 1 test
             </h1>
             <p className={
                 styles.intro
